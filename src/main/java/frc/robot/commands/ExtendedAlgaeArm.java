2b19ffc5a38991f9e18631d8e55f21e2570fe00f
stavit456 <stavfrenkel456@gmail.com>
2025-01-14 22:05:16 +0200

    I finished AlgaeArm.

---
+++ src/main/java/frc/robot/commands/ExtendedAlgaeArm.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeArm;
+
+public class ExtendedAlgaeArm extends Command {
+    private AlgaeArm algaeArm;
+
+    public ExtendedAlgaeArm(AlgaeArm algaeArm){
+        this.algaeArm = algaeArm;
+
+        addRequirements(algaeArm);
+    }
+
+    @Override
+    public void initialize() {
+        algaeArm.extend();
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return algaeArm.isExtended();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+
+}
---
+++ src/main/java/frc/robot/commands/RetractAlgaeArm.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeArm;
+
+public class RetractAlgaeArm extends Command {
+    private AlgaeArm algaeArm;
+
+    public RetractAlgaeArm(AlgaeArm algaeArm){
+        this.algaeArm = algaeArm;
+
+        addRequirements(algaeArm);
+    }
+
+    @Override
+    public void initialize() {
+        algaeArm.retract();
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return algaeArm.isRetracted();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
