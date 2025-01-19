72d474156577a9d6a530048c85eb24f9dfb3a044
tbens <tbenshal@gmail.com>
2025-01-19 20:09:35 +0200

    simple PID method for movement to angle

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,4 +5,10 @@ public class RobotMap {
     private RobotMap() {}
 
     public static final int ARM_CORAL_MOTOR = 1;
+    public static final double KP = 0;
+    public static final double KI = 0;
+    public static final double KD = 0;
+    public static final double IZONE = 0;
+    public static final double TOLORANCE = 0;
 }
+
--- src/main/java/frc/robot/subsystems/CoralArm.java
+++ src/main/java/frc/robot/subsystems/CoralArm.java
@@ -1,8 +1,12 @@
 package frc.robot.subsystems;
 
+
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.jni.CANSparkJNI;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -13,14 +17,35 @@ import java.util.concurrent.CancellationException;
 
 public class CoralArm extends SubsystemBase {
     private SparkMax sparkMax ;
-
+    private PIDController pid;
     private DigitalInput extendlimitswitch;
     private DigitalInput retractedLimitSwitch;
+    private AbsoluteEncoder encoder;
     public CoralArm(){
         sparkMax = new SparkMax(RobotMap.ARM_CORAL_MOTOR, SparkLowLevel.MotorType.kBrushless);
+        SparkMaxConfig config = new SparkMaxConfig();
+        final int START_PULSE_US = 1;
+        final int END_PULSE_US = 1024;
+        final double ZERO_OFFSET = 0.2;
+
+        config.absoluteEncoder.startPulseUs(START_PULSE_US).endPulseUs(END_PULSE_US).zeroOffset(ZERO_OFFSET);
+        encoder = sparkMax.getAbsoluteEncoder();
+        pid = new PIDController(RobotMap.KP,RobotMap.KI,RobotMap.KD);
+        pid.setIZone(RobotMap.IZONE);
+        pid.setTolerance(RobotMap.TOLORANCE);
        this.extendlimitswitch = new DigitalInput(1);
 
         
+    }
+
+    public void moveToAngle(double angle){
+        // angle offsets logics
+        double speed = pid.calculate(getPosition(),angle);
+        // angle offsets logics
+        sparkMax.set(speed);
+    }
+    public double getPosition(){
+        return encoder.getPosition()*360;
     }
     public boolean isExtended(){
         return !extendlimitswitch.get();
